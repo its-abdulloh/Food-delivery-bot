faa6d95cffa58161b386f4925a4dc1fd6d269fa7
Abdulloh <foziljonovabdulloh64@gmail.com>
2026-06-18 12:44:56 +0500

    moved payment handlers to handlers/payments.py

---
+++ handlers/payment.py
@@ -0,0 +1,128 @@
+from aiogram import Router, F
+from aiogram.types import (
+    Message,
+    CallbackQuery,
+)
+
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from helpers import MENU
+from helpers import (
+    ADMIN_ID,
+    update_order_status,
+    get_order_user,
+    )
+
+from states.registration import Registration
+from states.checkout import Checkout
+from states.admin import AdminCancelOrder
+
+
+router = Router()
+
+#IF ADMIN CONFIRMS
+@router.callback_query(F.data.startswith("confirm:"))
+async def confirm(callback:CallbackQuery):
+    if callback.from_user.id != ADMIN_ID:
+        await callback.answer("Not allowed", show_alert=True)
+        return
+
+    order_id = int(callback.data.split(":")[1])
+
+    update_order_status(order_id,"confirmed")
+
+    user_id = get_order_user(order_id)
+    await callback.bot.send_message(
+        chat_id=user_id,
+        text=(
+            f"✅ Tolo'vingiz tasdiqlandi\n"
+        )
+    )
+
+    await callback.message.edit_caption(
+        callback.message.caption + "\n\n✅ Tasdiqlandi",
+        reply_markup=None
+    )
+
+    await callback.answer("Order confirmed")
+
+#IF ADMIN CANCELS
+@router.callback_query(F.data.startswith("cancel:"))
+async def cancel_order(callback: CallbackQuery, state: FSMContext):
+    if callback.from_user.id != ADMIN_ID:
+        await callback.answer("Not allowed", show_alert=True)
+        return
+
+    order_id = int(callback.data.split(":")[1])
+
+    await state.update_data(
+        order_id=order_id,
+        admin_chat_id=callback.message.chat.id,
+        admin_message_id=callback.message.message_id,
+        original_caption=callback.message.caption
+    )
+
+    
+
+    await callback.message.answer(
+        f"Nega buyurtma #{order_id}ni bekor qilyapsiz?"
+    )
+
+    await state.set_state(AdminCancelOrder.waiting_for_reason)
+
+    await callback.answer()
+
+#IF INVALID REASON
+@router.message(AdminCancelOrder.waiting_for_reason)
+async def invalid_reason(message: Message):
+    await message.answer(
+        "Iltimos bekor qilish sababini matn ko'rinishida yuboring."
+    )
+
+#ASKING FOR WHY ADMIN CANCELED
+@router.message(AdminCancelOrder.waiting_for_reason,F.text)
+async def process_cancel_reason(
+    message: Message,
+    state: FSMContext
+):
+    reason = message.text
+
+    data = await state.get_data()
+    order_id = data["order_id"]
+    admin_chat_id = data["admin_chat_id"]
+    admin_message_id = data["admin_message_id"]
+    caption = data["original_caption"]
+
+    update_order_status(order_id, "CANCELED")
+
+    user_id = get_order_user(order_id)
+
+    await message.bot.send_message(
+        user_id,
+        f"""
+❌ To'lovingiz bekor qilindi.
+
+Sabab:
+{reason}
+"""
+    )
+
+    await message.bot.edit_message_caption(
+        chat_id=admin_chat_id,
+        message_id=admin_message_id,
+        caption=caption + f"""
+
+❌ Buyurtma bekor qilindi
+
+Sabab:
+{reason}
+""",
+        reply_markup=None
+    )
+
+    await message.answer(
+        f"Buyurtma #{order_id} bekor qilindi."
+    )
+
+    await state.clear()
--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import asyncio
 from aiogram import Bot
 from config import TOKEN
+
 from bot import dp   # import dispatcher from bot.py
+
+
 from handlers.start import router as start_router
 from handlers.registration import router as registration_router
 from handlers.menu import router as menu_router
 from handlers.cart import router as cart_router
 from handlers.checkout import router as checkout_router
+from handlers.payment import router as payment_router
 
-dp.include_router(start_router)
-dp.include_router(registration_router)
-dp.include_router(menu_router)
-dp.include_router(cart_router)
-dp.include_router(checkout_router)
+
 
 async def main():
+    dp.include_router(start_router)
+    dp.include_router(registration_router)
+    dp.include_router(menu_router)
+    dp.include_router(cart_router)
+    dp.include_router(checkout_router)
+    dp.include_router(payment_router)
+
     bot = Bot(token=TOKEN)
     await dp.start_polling(bot)
 
